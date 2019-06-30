30c9553c9ce94fe4a1acb3259ec79fceb119246f
Carina2207 <fiodorov.flp@gmail.com>
2019-06-30 18:26:04 +0300

    show the list of posts

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # commentariu
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+from django.shortcuts import render
+from .models import Post
+Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')from django.shortcuts import render
     
